e685e2d1d34ce126c93ffc1f52c1f6902ea64742
Darren Embry <dse@webonastick.com>
2018-11-16 21:14:04 -0500

    fixes

--- lib/My/Printable/Element/Line.pm
+++ lib/My/Printable/Element/Line.pm
@@ -12,10 +12,10 @@ use base qw(My::Printable::Element);
 
 sub draw {
     my ($self) = @_;
-    my $x1 = $self->x1 // $self->leftMarginX;
-    my $x2 = $self->x2 // $self->rightMarginX;
-    my $y1 = $self->y1 // $self->topMarginY;
-    my $y2 = $self->y2 // $self->bottomMarginY;
+    my $x1 = $self->x1 // $self->document->leftMarginX;
+    my $x2 = $self->x2 // $self->document->rightMarginX;
+    my $y1 = $self->y1 // $self->document->topMarginY;
+    my $y2 = $self->y2 // $self->document->bottomMarginY;
     my $cssClass = $self->cssClass // "blue line";
     my $line = $self->createSVGLine(x1 => $x1, y1 => $y1, x2 => $x2, y2 => $y2, cssClass => $cssClass);
     $self->appendSVGLine($line);
