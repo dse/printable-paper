bf915d8d8c852e4954b2e625c0c3e8849bacf667
Darren Embry <dse@webonastick.com>
2018-11-16 21:13:27 -0500

    fixes

--- lib/My/Printable/PointSeries.pm
+++ lib/My/Printable/PointSeries.pm
@@ -6,8 +6,8 @@ use v5.10.0;
 use lib "$ENV{HOME}/git/dse.d/perl-class-thingy/lib";
 use Class::Thingy;
 
-public 'start';
-public 'end';
+public 'startPoint';
+public 'endPoint';
 public 'spacing';
 public 'origin';
 public 'min';
@@ -18,19 +18,19 @@ our $FUDGE = 0.0001;
 sub init {
     my ($self) = @_;
     if (defined $self->origin && defined $self->spacing) {
-        if (defined $self->min && !defined $self->start) {
+        if (defined $self->min && !defined $self->startPoint) {
             my $start = $self->origin;
             while ((my $new_start = $start - $self->spacing) >= ($self->min - $FUDGE)) {
                 $start = $new_start;
             }
-            $self->start($start);
+            $self->startPoint($start);
         }
-        if (defined $self->max && !defined $self->end) {
+        if (defined $self->max && !defined $self->endPoint) {
             my $end = $self->origin;
             while ((my $new_end = $end + $self->spacing) <= ($self->max + $FUDGE)) {
                 $end = $new_end;
             }
-            $self->end($end);
+            $self->endPoint($end);
         }
     }
 }
@@ -39,12 +39,14 @@ sub extendAhead {
     my ($self, $steps) = @_;
     $steps //= 1;
     $self->max($self->max + $self->spacing * $steps);
+    $self->endPoint($self->endPoint + $self->spacing * $steps);
 }
 
 sub extendBehind {
     my ($self, $steps) = @_;
     $steps //= 1;
     $self->min($self->min - $self->spacing * $steps);
+    $self->startPoint($self->startPoint - $self->spacing * $steps);
 }
 
 sub chopBehind {
@@ -52,6 +54,9 @@ sub chopBehind {
     return unless defined $min;
     while ($self->min < ($min - $FUDGE)) {
         $self->min($self->min + $self->spacing);
+    }
+    while ($self->startPoint < ($min - $FUDGE)) {
+        $self->startPoint($self->startPoint + $self->spacing);
     }
 }
 
@@ -61,12 +66,15 @@ sub chopAhead {
     while ($self->max > ($max + $FUDGE)) {
         $self->max($self->max - $self->spacing);
     }
+    while ($self->endPoint > ($max + $FUDGE)) {
+        $self->endPoint($self->endPoint - $self->spacing);
+    }
 }
 
 sub getPoints {
     my ($self) = @_;
     my @points;
-    for (my $point = $self->min; $point <= ($self->max + $FUDGE); $point += $self->spacing) {
+    for (my $point = $self->startPoint; $point <= ($self->endPoint + $FUDGE); $point += $self->spacing) {
         push(@points, $point);
     }
     return @points;
