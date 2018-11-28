bad4771b77baa4f43a059c87fc94501e4f3acf14
Darren Embry <dse@webonastick.com>
2018-11-27 20:10:40 -0500

    fixes

--- lib/My/Printable/Ruling/Quadrille.pm
+++ lib/My/Printable/Ruling/Quadrille.pm
@@ -6,6 +6,8 @@ use v5.10.0;
 use lib "$ENV{HOME}/git/dse.d/perl-class-thingy/lib";
 use Class::Thingy;
 
+use My::Printable::Element::Grid;
+
 use lib "$ENV{HOME}/git/dse.d/printable-paper/lib";
 use base 'My::Printable::Ruling';
 
