5056bca3089ed433db4b82328ec825726010cfb7
Darren Embry <dse@webonastick.com>
2018-11-28 00:54:20 -0500

    inkscape shell

---
+++ lib/My/Printable/Util/InkscapeShell.pm
@@ -0,0 +1,27 @@
+package My::Printable::Util::InkscapeShell;
+use warnings;
+use strict;
+use v5.10.0;
+
+use lib "$ENV{HOME}/git/dse.d/perl-class-thingy/lib";
+use Class::Thingy;
+
+use Expect qw();
+
+public 'expect', lazy => 1, builder => sub {
+    my @cmd = ('inkscape', '--shell');
+    my $expect = Expect->new();
+    $expect->raw_pty(0);
+    $expect->spawn(@cmd) or die("Cannot spawn @cmd: $!\n");
+    $expect->expect(5, ">");
+    return $expect;
+};
+
+sub cmd {
+    my ($self, $cmd) = @_;
+    $cmd =~ s{\R\z}{};          # safer chomp
+    $self->expect->send("$cmd\n");
+    $self->expect->expect(5, ">");
+}
+
+1;
