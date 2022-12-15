dd9e81c7653e25d1f409875c4d1afa137160d4c1
Darren Embry <dse@webonastick.com>
2022-12-15 00:50:19 -0500

    now supports fractions

--- lib/My/RuledPaper/Dimen.pm
+++ lib/My/RuledPaper/Dimen.pm
@@ -15,10 +15,27 @@ use My::RuledPaper::Constants qw($RE_UNIT $RE_NUM %UNITS);
 
 sub parseDimen {
     my ($str) = @_;
-    if ($str =~ m{^(?<num>$RE_NUM) \s* (?<unit>$RE_UNIT)$}xi) {
-        my $num = $+{num};
-        my $unit = $+{unit};
-        return $num * $UNITS{lc($unit)};
+    if ($str =~ m{^
+                  (?<num>$RE_NUM)
+                  (?:
+                      \s*
+                      /
+                      \s*
+                      (?<denom>$RE_NUM)
+                  )?
+                  (?:
+                      \s*
+                      (?<unitName>$RE_UNIT)
+                  )?
+                  $}xi) {
+        my $num      = $+{num};
+        my $denom    = $+{denom} // 1;
+        my $unitName = $+{unitName};
+        my $unit     = defined $unitName ? $UNITS{lc($unitName)} : undef;
+        if (defined $unitName && !defined $unit) {
+            return;
+        }
+        return $num / ($denom // 1) * ($unit // 1);
     }
     if ($str =~ m{^$RE_NUM$}) {
         return 0 + $str;
