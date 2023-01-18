eed87378847aa154c83c92a37e13914e438cee30
Darren Embry <dse@webonastick.com>
2023-01-18 00:09:25 -0500

    comments and fixes

--- lib/My/RuledPaper/Parse/GridStyle.pm
+++ lib/My/RuledPaper/Parse/GridStyle.pm
@@ -4,9 +4,13 @@ use strict;
 
 # example:
 #     2mm,3,12
-# feint lines every 2mm
+#     feint lines every 2mm
 #     minor lines every 3 feint lines
 #     major lines every 12 feint lines
+# example 2:
+#     2mm,4
+#     feint lines every 2mm
+#     **major** lines every 4 feint lines
 
 use base 'Exporter';
 
@@ -32,14 +36,16 @@ sub parseGridStyle {
     die("invalid grid style: $style ($major is not a valid major)\n") if defined $major && !looks_like_number($major);
     $minor = round($minor) if defined $minor;
     $major = round($major) if defined $major;
-    die("invalid grid style: $style (minor $minor is too small)") if $minor < 1;
-    die("invalid grid style: $style (minor $minor is too large)") if $minor > 32;
-    die("invalid grid style: $style (major $major is too small)") if $major < 1;
-    die("invalid grid style: $style (major $major is too large)") if $major > 32;
-    die("invalid grid style: $style (major $major is not greater than minor $minor)") if $major <= $minor;
-    die("invalid grid style: $style (major $major is not a multiple of minor $minor)") if $major % $minor != 0;
+    die("invalid grid style: $style (minor $minor is too small)") if defined $minor && $minor < 1;
+    die("invalid grid style: $style (minor $minor is too large)") if defined $minor && $minor > 32;
+    die("invalid grid style: $style (major $major is too small)") if defined $major && $major < 1;
+    die("invalid grid style: $style (major $major is too large)") if defined $major && $major > 32;
+    die("invalid grid style: $style (major $major is not greater than minor $minor)") if defined $major && defined $minor && $major <= $minor;
+    die("invalid grid style: $style (major $major is not a multiple of minor $minor)") if defined $major && defined $minor && $major % $minor != 0;
     if (!defined $major) {
         $major = $minor;
+        $minor = undef;
+    } elsif ($minor == $major) { # in case of something like "2mm,4,4"
         $minor = undef;
     }
     return ($spacing, $minor, $major);
